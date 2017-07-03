8136c08594013f13fe8f7285e4fc6cad49951eb2
dIJKLMstra <1500012917@pku.edu.cn>
2017-07-03 20:37:23 +0800

    Update Grid.h

--- Grid.h
+++ Grid.h
@@ -7,40 +7,47 @@
 #include <iostream>
 
 #include "vector.hpp"
-#define particle_Grid_Len 40
 
+/* 粒子网格的边长 */
+#define particle_Grid_Len 50
+
+/* 每两个粒子之间的距离 */
 const double h = 1.0 / particle_Grid_Len;
 
-/* 粒子结构 */
-
+/* 
+ *	单个粒子类
+ *  里面存储每个粒子的属性
+ */
 struct Particle
 {
-	unsigned int  r, g, b;      /* 粒子的颜色 */
-	double vx, vy, vz;          /* 粒子的当前速度 */
+	unsigned int  r, g, b;				/* 粒子的颜色 */
+	double vx, vy, vz;					/* 粒子的当前速度 */
 	double vStarX, vStarY, vStarZ;
-	double duration;			/* 粒子持续时间 */
-	double size;                /* 粒子尺寸 */
-	double temperature;         /* 粒子温度 */
-	double density;             /* 粒子密度 */
-	bool border;                 /* 是否为边界 */
+	double duration;					/* 粒子持续时间 */
+	double size;						/* 粒子尺寸 */
+	double temperature;					/* 粒子温度 */
+	double density;						/* 粒子密度 */
+	double p;							/* 粒子压强 */
+	bool border;						/* 是否为外部边界 */
+	bool isOccupied;					/* 是否被物体占据 */
 	Vec3 advectionTerm;
-	double force[3];
+	double force[3];					/* 粒子当前受到的外力 */
 };
 
 /* 粒子网格类 */
 class Grid
 {
-public:
-	Particle***   particle;               /* 粒子指针 */
-										  //int         particle_Cnt;         /* 粒子数目 */
-	double      temp_avg;            /* 温度条件 */
 
-									 /* 构造函数 */
+public:
+	Particle***   particle;             /* 粒子指针 */
+	double      temp_avg;				/* 所有粒子的平均温度 */
+
+	/* 构造函数 */
 	Grid() {
 		particle = NULL;
-		//particle_Cnt = 0;
 		temp_avg = 0;
 	}
+
 	/* 析构函数 */
 	~Grid() {
 		for (int i = 0; i < particle_Grid_Len; i++)
@@ -59,8 +66,20 @@ public:
 
 	/* 创建粒子数组 */
 	void CreateGrid(long num) {
-		if (particle)
+		if (particle) {
+			for (int i = 0; i < particle_Grid_Len; i++)
+				for (int j = 0; j < particle_Grid_Len; j++) {
+					delete[] particle[i][j];
+					particle[i][j] = NULL;
+				}
+
+			for (int i = 0; i < particle_Grid_Len; i++) {
+				delete[] particle[i];
+				particle[i] = NULL;
+			}
 			delete[] particle;
+			particle = NULL;
+		}
 
 		particle = (Particle***)new Particle**[particle_Grid_Len];
 
@@ -73,10 +92,9 @@ public:
 				particle[i][j] = new Particle[particle_Grid_Len];
 			}
 
-
 	}
 
-	/* 设置和获取颜色属性 */
+	/* 设置及获取粒子的颜色 */
 	void SetAllColor(GLint r, GLint g, GLint b) {
 		for (int i = 0; i < particle_Grid_Len; i++)
 			for (int j = 0; j < particle_Grid_Len; j++)
@@ -111,7 +129,7 @@ public:
 		return false;
 	}
 
-	/* 设置和获取速度属性 */
+	/* 设置及获取粒子当前速度 */
 	void SetAllVelocity(GLdouble vx, GLdouble vy, GLdouble vz) {
 		for (int i = 0; i < particle_Grid_Len; i++)
 			for (int j = 0; j < particle_Grid_Len; j++)
@@ -146,6 +164,7 @@ public:
 		return false;
 	}
 
+	/* 设置V*与V相同 */
 	bool SetVstar(int i, int j, int k) {
 		if (i >= 0 && i < particle_Grid_Len
 			&& j >= 0 && j < particle_Grid_Len
@@ -158,7 +177,7 @@ public:
 		return false;
 	}
 
-	/* 设置和获取持续时间 */
+	/* 设置及获取粒子持续时间 */
 	void SetAllDuration(GLdouble duration) {
 		for (int i = 0; i < particle_Grid_Len; i++)
 			for (int j = 0; j < particle_Grid_Len; j++)
@@ -187,7 +206,7 @@ public:
 		return false;
 	}
 
-	/* 设置和获取尺寸属性 */
+	/* 设置及获取粒子尺寸 */
 	void SetAllSize(GLdouble size) {
 		for (int i = 0; i < particle_Grid_Len; i++)
 			for (int j = 0; j < particle_Grid_Len; j++)
@@ -206,7 +225,7 @@ public:
 		return false;
 	}
 
-	int GetSize(GLint i, GLint j, GLint k, GLdouble &size) {
+	bool GetSize(GLint i, GLint j, GLint k, GLdouble &size) {
 		if (i >= 0 && i < particle_Grid_Len
 			&& j >= 0 && j < particle_Grid_Len
 			&& k >= 0 && k < particle_Grid_Len) {
@@ -216,7 +235,7 @@ public:
 		return false;
 	}
 
-	/* 设置和获取温度信息 */
+	/* 设置及获取粒子温度 */
 	void SetAllTemperature(GLdouble temp) {
 		for (int i = 0; i < particle_Grid_Len; i++)
 			for (int j = 0; j < particle_Grid_Len; j++)
@@ -235,7 +254,7 @@ public:
 		return false;
 	}
 
-	int GetTemperature(GLint i, GLint j, GLint k, GLdouble &temp) {
+	bool GetTemperature(GLint i, GLint j, GLint k, GLdouble &temp) {
 		if (i >= 0 && i < particle_Grid_Len
 			&& j >= 0 && j < particle_Grid_Len
 			&& k >= 0 && k < particle_Grid_Len) {
@@ -245,6 +264,7 @@ public:
 		return false;
 	}
 
+	/* 计算网格内所有粒子的平均温度 */
 	void CalAvgTemperature() {
 		for (int i = 1; i < particle_Grid_Len - 1; i++)
 			for (int j = 1; j < particle_Grid_Len - 1; j++)
@@ -254,7 +274,7 @@ public:
 		temp_avg /= (particle_Grid_Len - 2)* (particle_Grid_Len - 2)* (particle_Grid_Len - 2);
 	}
 
-	/* 设置和获取密度信息 */
+	/* 设置及获取粒子密度 */
 	void SetAllDensity(GLdouble density) {
 		for (int i = 0; i < particle_Grid_Len; i++)
 			for (int j = 0; j < particle_Grid_Len; j++)
@@ -273,7 +293,7 @@ public:
 		return false;
 	}
 
-	int GetDensity(GLint i, GLint j, GLint k, GLdouble &density) {
+	bool GetDensity(GLint i, GLint j, GLint k, GLdouble &density) {
 		if (i >= 0 && i < particle_Grid_Len
 			&& j >= 0 && j < particle_Grid_Len
 			&& k >= 0 && k < particle_Grid_Len) {
@@ -283,23 +303,35 @@ public:
 		return false;
 	}
 
-	/* 获取粒子数组地址 */
-	//Particle *GetParticle() { 
-	//	return particle; 
-	//}
+	/* 烟雾生成区继续补充烟雾密度 */
+	void replenishDensity() {
+		for (int i = 1; i < particle_Grid_Len - 1; i++)
+			for (int j = 1; j < particle_Grid_Len - 1; j++)
+				for (int k = 1; k < particle_Grid_Len - 1; k++) {
 
-	/* 获得粒子的数目 */
-	//int GetParticleCnt() { return particle_Cnt; }
+					double x = 20 * ((double)i / particle_Grid_Len) - 10;
+					double y = 5 * ((double)j / particle_Grid_Len);
+					double z = 20 * ((double)k / particle_Grid_Len) - 10;
+
+					/* 烟雾生成区继续生成烟雾 */
+					if (x * x + z * z < 1 && y < 1) {
+						particle[i][j][k].density += (1 - x * x - z * z) * (1 - y) * (rand() % 1000 / 1000.0);
+						if (particle[i][j][k].density > 1)
+							particle[i][j][k].density = 1;
+					}
+				}
+	}
+
 
 	/* 设置粒子的所有属性 */
-	int SetAllData(int i, int j, int k,                /* 下标 */
-		GLint r, GLint g, GLint b,                /* 颜色 */
-		GLdouble vx, GLdouble vy, GLdouble vz,    /* 速度 */
-		GLdouble vStarX,GLdouble vStarY,GLdouble vStarZ,
-		GLdouble size,                              /* 大小 */
-		GLdouble duration,                          /* 持续时间 */
-		GLdouble temperature,                       /* 温度 */
-		GLdouble density,                            /* 密度 */
+	int SetAllData(int i, int j, int k,						/* 下标 */
+		GLint r, GLint g, GLint b,							/* 颜色 */
+		GLdouble vx, GLdouble vy, GLdouble vz,				/* 速度 */
+		GLdouble vStarX, GLdouble vStarY, GLdouble vStarZ,
+		GLdouble size,										/* 尺寸 */
+		GLdouble duration,									/* 持续时间 */
+		GLdouble temperature,								/* 温度 */
+		GLdouble density,									/* 密度 */
 		Vec3 advectionTerm
 		) {
 		if (i >= 0 && i < particle_Grid_Len
@@ -318,21 +350,21 @@ public:
 			particle[i][j][k].duration = duration;
 			particle[i][j][k].temperature = temperature;
 			particle[i][j][k].density = density;
-			particle[i][j][k].advectionTerm= advectionTerm;
+			particle[i][j][k].advectionTerm = advectionTerm;
 			return true;
 		}
 		return false;
 	}
 
 	/* 获得粒子所有的属性 */
-	int GetAllData(int i, int j, int k,              /* 下标 */
-		GLint &r, GLint &g, GLint &b,                /* 颜色 */
-		GLdouble &vx, GLdouble &vy, GLdouble &vz,    /* 速度 */
-		GLdouble &vStarX,GLdouble &vStarY,GLdouble &vStarZ,
-		GLdouble &size,                              /* 大小 */
-		GLdouble &duration,                          /* 持续时间 */
-		GLdouble &temperature,                       /* 温度 */
-		GLdouble &density,                            /* 密度 */
+	int GetAllData(int i, int j, int k,							/* 下标 */
+		GLint &r, GLint &g, GLint &b,							/* 颜色 */
+		GLdouble &vx, GLdouble &vy, GLdouble &vz,				/* 速度 */
+		GLdouble &vStarX, GLdouble &vStarY, GLdouble &vStarZ,
+		GLdouble &size,											/* 尺寸 */
+		GLdouble &duration,										/* 持续时间 */
+		GLdouble &temperature,									/* 温度 */
+		GLdouble &density,										/* 密度 */
 		Vec3 &advectionTerm
 		) {
 
@@ -368,9 +400,9 @@ public:
 		for (int i = 0; i < particle_Grid_Len; i++)
 			for (int j = 0; j < particle_Grid_Len; j++)
 				for (int k = 0; k < particle_Grid_Len; k++) {
-					SetAllData(i, j, k,100,100,100, 0, 0, 0,
-						0, 0, 0, double(1.5/particle_Grid_Len), 0, 0, 0, zero);
-					
+					SetAllData(i, j, k, 100, 100, 100, 0, 0, 0,
+						0, 0, 0, double(1.5 / particle_Grid_Len), 0, 0, 0, zero);
+
 					for (int m = 0; m < 3; m++)
 						particle[i][j][k].force[m] = 0;
 
@@ -378,23 +410,75 @@ public:
 					double y_loca = (j / particle_Grid_Len) - 0.5;
 					double z_loca = (k / particle_Grid_Len) - 0.5;
 
+					int u_border = 0.35 * particle_Grid_Len;
+					int b_border = 0.65 * particle_Grid_Len;
+
 					/* 边界的初始化 */
 					if (i == 0 || i == particle_Grid_Len - 1
 						|| j == 0 || j == particle_Grid_Len - 1
 						|| k == 0 || k == particle_Grid_Len - 1)
 						particle[i][j][k].border = true;
 
-					else if ((x_loca > -0.15 && i < x_loca < 0.15)
-						|| (y_loca > -0.15 && y_loca < 0.15)
-						|| (z_loca > -0.15 && z_loca < 0.15))
+					else if (((i == u_border || i == b_border) && j > u_border
+						&& j < b_border && k > u_border && k < b_border)
+						|| ((j == u_border || j == b_border) && i > u_border
+							&& i < b_border && k > u_border && k < b_border)
+						|| ((k == u_border || k == b_border) && j > u_border
+							&& j < b_border && i > u_border && i < b_border))
 						particle[i][j][k].border = true;
 
 					else
 						particle[i][j][k].border = false;
+
+					/* 物体边界的初始化 */
+					if (i >= u_border && i <= b_border
+						&& j >= u_border && j <= b_border
+						&& k >= u_border && k <= b_border)
+						particle[i][j][k].isOccupied = true;
+
+					else
+						particle[i][j][k].isOccupied = false;
 				}
 	}
 
+	/* 去掉一些边缘烟雾 */
+	bool isloateParticle(int i, int j, int k) {
+		int particle_Cnt = 6;
+		if (particle[i][j][k].density > 0){
+			if (particle[i - 1][j][k].density == 0
+				|| particle[i - 1][j][k].duration < 0
+				|| particle[i - 1][j][k].border)
+				particle_Cnt--;
+			if (particle[i + 1][j][k].density == 0
+				|| particle[i + 1][j][k].duration < 0
+				|| particle[i + 1][j][k].border)
+				particle_Cnt--;
+			if (particle[i][j - 1][k].density == 0
+				|| particle[i][j - 1][k].duration < 0
+				|| particle[i][j - 1][k].border)
+				particle_Cnt--;
+			if (particle[i][j + 1][k].density == 0
+				|| particle[i][j + 1][k].duration < 0
+				|| particle[i][j + 1][k].border)
+				particle_Cnt--;
+			if (particle[i][j][k - 1].density == 0
+				|| particle[i][j][k - 1].duration < 0
+				|| particle[i][j][k - 1].border)
+				particle_Cnt--;
+			if (particle[i][j][k + 1].density == 0
+				|| particle[i][j][k + 1].duration < 0
+				|| particle[i][j][k + 1].border)
+				particle_Cnt--;
+			if(particle_Cnt <= 1)
+				particle[i][j][k].density = 0;
+			return true;
+		}
+		return false;
+	}
+
+	/* 计算漩涡量 */
 	bool calculateVorticity(int i, int j, int k, double vort[3]) {
+		/* 每个方向上的漩涡分量通过周围粒子的速度分量差值计算得到 */
 		if (i > 0 && i < particle_Grid_Len - 1
 			&& j > 0 && j < particle_Grid_Len - 1
 			&& k > 0 && k < particle_Grid_Len - 1) {
@@ -417,6 +501,7 @@ public:
 		return false;
 	}
 
+	/* 计算该粒子所受外力 */
 	bool calculateForce(int i, int j, int k) {
 		if (i > 0 && i < particle_Grid_Len - 1
 			&& j > 0 && j < particle_Grid_Len - 1
@@ -434,13 +519,14 @@ public:
 			double vort_loca[3] = {};
 			//double force[3] = {};
 
-
+			/* 计算温度与密度对外力的影响 */
 			for (int m = 0; m < 3; m++) {
 				particle[i][j][k].force[m] = 0;
 				particle[i][j][k].force[m] += beta * (particle[i][j][k].temperature - temp_avg) * array_z[m];
 				particle[i][j][k].force[m] -= alpha * particle[i][j][k].density * array_z[m];
 			}
 
+			/* 计算涡旋约束力对外力的影响 */
 			calculateVorticity(i, j, k, vorticity);
 			calculateVorticity(i + 1, j, k, x_next_vort);
 			calculateVorticity(i, j + 1, k, y_next_vort);
@@ -473,3 +559,10 @@ public:
 	}
 
 } old_smoke, gen_smoke;
+/* 
+ * 创建两个网格类对象
+ * old_smoke 是已经初始化过数据或通过计算得到的烟雾网格状态
+ * gen_smoke 是需要通过old_smoke的状态计算得到的最新的烟雾网格状态
+ * 每次计算完成后会将gen_smoke的数据传递给old_smoke
+ * 不断循环进行后面的计算
+ */
